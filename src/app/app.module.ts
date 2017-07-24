9b36df393afa3afd24eebd41ce75edda78e95e60
Cory Ginsberg <cory.ginsberg@vistex.com>
2017-07-24 15:21:53 -0500

    Initial commit. Added posts, navigation, home and footer components. Navigation and footer are loaded on every page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent
   ],
+
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
