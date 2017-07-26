69bb0a58f88e0cc64d4e96364407f625df181e30
Cory Ginsberg <cory.ginsberg@vistex.com>
2017-07-26 12:08:21 -0500

    rewrote dropdown and cards so be looped through instead of hard-coded.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,15 +2,56 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'vistex-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'vistex-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    solutions = ['Go-to-Market Suite',
+        'Solution for SAP',
+        'channelConduit Suite',
+        'Counterpoint Suite',
+        'VIBES'];
+    industries = ['Agriculture',
+        'Chemical',
+        'Foodservice',
+        'Manufaturing',
+        'Mill Products',
+        'Retail',
+        'Wholesale Distribution',
+        'Automotive',
+        'Consumer Products',
+        'High Tech',
+        'Life Science',
+        'Music, Media & Licensing',
+        'Telecommunications'];
+    services = ['Analytics',
+        'CRM Integration',
+        'Program Delivery',
+        'Solution Delivery',
+        'Customer Support',
+        'Global Payments',
+        'Strategy',
+        'Training'];
 
-  ngOnInit() {
-  }
+    ddIndustriesTtl = 'Industries';
+    ddServicesTtl = 'Services';
+    ddSolutionsTtl = 'Solutions';
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    toggleIndustry(industry) {
+        this.ddIndustriesTtl = industry;
+    }
+    toggleService(service) {
+        this.ddServicesTtl = service;
+    }
+    toggleSolution(solution) {
+        this.ddSolutionsTtl = solution;
+    }
 
 }
